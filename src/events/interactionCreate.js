36c962e1e1fc3ff5345bdc41e7910df65496e548
Théo A. S. <theosevero9@gmail.com>
2026-01-31 23:20:50 -0300

    refactor: update interactionCreate to support new commands

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -4,6 +4,20 @@ const { logger } = require('../utils/logger');
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction) {
+    if (interaction.isAutocomplete()) {
+      const command = interaction.client.commands.get(interaction.commandName);
+
+      if (!command?.autocomplete) return;
+
+      try {
+        await command.autocomplete(interaction);
+      } catch (error) {
+        logger.error('Error while running autocomplete.', error);
+      }
+
+      return;
+    }
+
     if (!interaction.isChatInputCommand()) return;
 
     const command = interaction.client.commands.get(interaction.commandName);
